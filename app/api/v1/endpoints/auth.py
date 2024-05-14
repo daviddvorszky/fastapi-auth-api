bb0a8f28912e8abb29b2775994e3cf2538b0d7b2
daviddvorszky <dv98david@gmail.com>
2024-05-14 22:49:24 +0200

    Fix import

--- app/api/v1/endpoints/auth.py
+++ app/api/v1/endpoints/auth.py
@@ -3,7 +3,7 @@ from fastapi import APIRouter, Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordRequestForm
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from app import crud
+from app.crud.user import crud_user
 from app.schemas import token as token_schema
 from app.core import security
 from app.core.config import settings
@@ -13,7 +13,7 @@ router = APIRouter()
 
 @router.post("/token", response_model=token_schema.Token)
 async def login_for_access_token(db: AsyncSession = Depends(get_db), form_data: OAuth2PasswordRequestForm = Depends()):
-    user = await crud.crud_user.authenticate(db, email=form_data.username, password=form_data.password)
+    user = await crud_user.authenticate(db, email=form_data.username, password=form_data.password)
     if not user:
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
