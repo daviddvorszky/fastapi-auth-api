67dfc7f5fedd7445f6a0689d1eef320f973dca4f
daviddvorszky <dv98david@gmail.com>
2024-05-14 22:10:55 +0200

    Fix imports

--- app/api/v1/endpoints/auth.py
+++ app/api/v1/endpoints/auth.py
@@ -3,14 +3,15 @@ from fastapi import APIRouter, Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordRequestForm
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from app import crud, schemas, models
+from app import crud
+from app.schemas import token as token_schema
 from app.core import security
 from app.core.config import settings
 from app.db.session import get_db
 
 router = APIRouter()
 
-@router.post("/token", response_model=schemas.Token)
+@router.post("/token", response_model=token_schema.Token)
 async def login_for_access_token(db: AsyncSession = Depends(get_db), form_data: OAuth2PasswordRequestForm = Depends()):
     user = await crud.crud_user.authenticate(db, email=form_data.username, password=form_data.password)
     if not user:
--- app/api/v1/endpoints/users.py
+++ app/api/v1/endpoints/users.py
@@ -1,13 +1,14 @@
 from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from app import crud, schemas
+from app import crud
+from app.schemas import user as user_schema
 from app.db.session import get_db
 
 router = APIRouter()
 
-@router.post("/", response_model=schemas.User)
-async def create_user(user_in: schemas.UserCreate, db: AsyncSession = Depends(get_db)):
+@router.post("/", response_model=user_schema.User)
+async def create_user(user_in: user_schema.UserCreate, db: AsyncSession = Depends(get_db)):
     user = await crud.crud_user.get_user_by_email(db, email=user_in.email)
     if user:
         raise HTTPException(
