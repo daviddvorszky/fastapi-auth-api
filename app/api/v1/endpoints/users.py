b6f3614f618aa84328700a6ecce2bd552e436c2c
daviddvorszky <dv98david@gmail.com>
2024-05-14 23:14:32 +0200

    Fix import

--- app/api/v1/endpoints/users.py
+++ app/api/v1/endpoints/users.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from app import crud
+from app.crud.user import crud_user
 from app.schemas import user as user_schema
 from app.db.session import get_db
 
@@ -9,11 +9,11 @@ router = APIRouter()
 
 @router.post("/", response_model=user_schema.User)
 async def create_user(user_in: user_schema.UserCreate, db: AsyncSession = Depends(get_db)):
-    user = await crud.crud_user.get_user_by_email(db, email=user_in.email)
+    user = await crud_user.get_user_by_email(db, email=user_in.email)
     if user:
         raise HTTPException(
             status_code=status.HTTP_409_CONFLICT,
             detail="The user with this email already exists.",
         )
-    user = await crud.crud_user.create_user(db, user_in=user_in)
+    user = await crud_user.create_user(db, user_in=user_in)
     return user
