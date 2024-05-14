ab6da8541066f13478949a7feda800660c2a7b09
daviddvorszky <dv98david@gmail.com>
2024-05-14 22:49:12 +0200

    Add user validation functionality

--- app/crud/user.py
+++ app/crud/user.py
@@ -1,8 +1,10 @@
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.future import select
+from sqlalchemy.orm import Session
 
 from app.models.user import User
 from app.schemas.user import UserCreate
+from app.core.security import verify_password, get_password_hash
 
 class CRUDUser:
     async def get_user(self, db: AsyncSession, user_id: int):
@@ -14,10 +16,19 @@ class CRUDUser:
         return result.scalars().first()
 
     async def create_user(self, db: AsyncSession, user_in: UserCreate):
-        db_user = User(email=user_in.email, hashed_password=user_in.hashed_password)
+        hashed_password = get_password_hash(user_in.password)
+        db_user = User(email=user_in.email, hashed_password=hashed_password)
         db.add(db_user)
         await db.commit()
         await db.refresh(db_user)
         return db_user
+
+    async def authenticate(self, db: AsyncSession, email: str, password: str):
+        user = await self.get_user_by_email(db, email=email)
+        if not user:
+            return None
+        if not verify_password(password, user.hashed_password):
+            return None
+        return user
 
 crud_user = CRUDUser()
