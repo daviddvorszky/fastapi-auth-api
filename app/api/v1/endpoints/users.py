6dd0fb933e0764d28b72a55196d9f7eb2857ad34
daviddvorszky <dv98david@gmail.com>
2024-05-18 17:14:04 +0200

    Add user selection by username or email

--- app/api/v1/endpoints/users.py
+++ app/api/v1/endpoints/users.py
@@ -9,11 +9,13 @@ router = APIRouter()
 
 @router.post("/", response_model=user_schema.User)
 async def create_user(user_in: user_schema.UserCreate, db: AsyncSession = Depends(get_db)):
-    user = await crud_user.get_user_by_email(db, email=user_in.email)
+    user = await crud_user.get_user_by_username_or_email(db, username=user_in.username, email=user_in.email)
     if user:
+        existing_field = "username" if user.username == user_in.username else "email"
+        error_msg = f"A user with this {existing_field} already exists"
         raise HTTPException(
             status_code=status.HTTP_409_CONFLICT,
-            detail="The user with this email already exists.",
+            detail=error_msg,
         )
     user = await crud_user.create_user(db, user_in=user_in)
     return user
--- app/crud/user.py
+++ app/crud/user.py
@@ -1,3 +1,4 @@
+from sqlalchemy import or_
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.future import select
 from sqlalchemy.orm import Session
@@ -7,17 +8,21 @@ from app.schemas.user import UserCreate
 from app.core.security import verify_password, get_password_hash
 
 class CRUDUser:
-    async def get_user(self, db: AsyncSession, user_id: int):
+    async def get_user(self, db: AsyncSession, user_id: str):
         result = await db.execute(select(User).filter(User.id == user_id))
         return result.scalars().first()
 
     async def get_user_by_email(self, db: AsyncSession, email: str):
         result = await db.execute(select(User).filter(User.email == email))
         return result.scalars().first()
+    
+    async def get_user_by_username_or_email(self, db: AsyncSession, username: str, email: str):
+        result = await db.execute(select(User).filter(or_(User.username.like(username), User.email.like(email))))
+        return result.scalars().first()
 
     async def create_user(self, db: AsyncSession, user_in: UserCreate):
         hashed_password = get_password_hash(user_in.password)
-        db_user = User(email=user_in.email, hashed_password=hashed_password)
+        db_user = User(username=user_in.username, email=user_in.email, hashed_password=hashed_password)
         db.add(db_user)
         await db.commit()
         await db.refresh(db_user)
