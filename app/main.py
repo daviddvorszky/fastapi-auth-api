7246fbc6ef95443bae1792f0069a240ef79f136e
daviddvorszky <dv98david@gmail.com>
2024-05-20 10:52:15 +0200

    Add health check endpoint

--- app/main.py
+++ app/main.py
@@ -1,7 +1,8 @@
 from fastapi import FastAPI
-from app.api.v1.endpoints import auth, users
+from app.api.v1.endpoints import auth, users, health
 
 app = FastAPI()
 
 app.include_router(auth.router, prefix="/api/v1/auth", tags=["auth"])
 app.include_router(users.router, prefix="/api/v1/users", tags=["users"])
+app.include_router(health.router, prefix="/api/v1", tags=["health"])
