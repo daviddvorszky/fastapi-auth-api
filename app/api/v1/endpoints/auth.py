dc5525f81b42b83ba4c959e1f691ad5636a17cfb
daviddvorszky <dv98david@gmail.com>
2024-05-29 20:46:27 +0200

    Move register endpoint to auth

--- app/api/v1/endpoints/auth.py
+++ app/api/v1/endpoints/auth.py
@@ -11,10 +11,29 @@ from app.crud.user_session import crud_session
 from app.db.session import get_db
 from app.core.token_handler import verify_active_refresh_token, get_verify_access_token_dependency, invalidate_access_token
 from app.models.custom_types import ULIDType
-from app.schemas.user import UserLogin
+from app.schemas.user import User, UserLogin, UserCreate
 from app.schemas.user_session import SessionCreate
 
+
 router = APIRouter()
+
+
+@router.post("/register", response_model=User)
+async def create_user(
+    user_in: UserCreate, db: AsyncSession = Depends(get_db)
+):
+    user = await crud_user.get_user_by_username_or_email(
+        db, username=user_in.username, email=user_in.email
+    )
+    if user:
+        existing_field = "username" if user.username == user_in.username else "email"
+        error_msg = f"A user with this {existing_field} already exists"
+        raise HTTPException(
+            status_code=status.HTTP_409_CONFLICT,
+            detail=error_msg,
+        )
+    user = await crud_user.create_user(db, user_in=user_in)
+    return user
 
 
 @router.post("/login")
--- app/api/v1/endpoints/users.py
+++ app/api/v1/endpoints/users.py
@@ -8,19 +8,3 @@ from app.schemas import user as user_schema
 router = APIRouter()
 
 
-@router.post("/register", response_model=user_schema.User)
-async def create_user(
-    user_in: user_schema.UserCreate, db: AsyncSession = Depends(get_db)
-):
-    user = await crud_user.get_user_by_username_or_email(
-        db, username=user_in.username, email=user_in.email
-    )
-    if user:
-        existing_field = "username" if user.username == user_in.username else "email"
-        error_msg = f"A user with this {existing_field} already exists"
-        raise HTTPException(
-            status_code=status.HTTP_409_CONFLICT,
-            detail=error_msg,
-        )
-    user = await crud_user.create_user(db, user_in=user_in)
-    return user
